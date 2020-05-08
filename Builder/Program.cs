029e50b9557b2fa2a886f1d36fe5b191712b3c24
lucas letor <lucas.letor@gmail.com>
2020-05-08 17:20:28 +0200

    Add project files.

---
+++ Builder/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new MealBuilder();
+
+            var burgerMeal = builder.PrepareBurgerMeal();
+            Console.WriteLine("BurgerMeal: ");
+            burgerMeal.ShowItems();
+            Console.WriteLine($"Costs: {burgerMeal.GetCosts().ToString("c")}");
+
+            var chickenMeal = builder.PrepareChickenBurger();
+            Console.WriteLine("ChickenMeal: ");
+            chickenMeal.ShowItems();
+            Console.WriteLine($"Costs: {chickenMeal.GetCosts().ToString("c")}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
